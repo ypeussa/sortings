618c7213300c0dfaadc25d5a1d4c6e4efd95cfbb
AriPunavuori <38551509+AriPunavuori@users.noreply.github.com>
2023-04-25 13:03:12 +0300

    Description of sorting algorithm

--- Assets/Scripts/Sorters.cs
+++ Assets/Scripts/Sorters.cs
@@ -47,7 +47,36 @@ public class Sorters : MonoBehaviour
 
     private void Sort()
     {
+        // The algorithm consists of array indexes, the search areas, sort areas
+        // and the logic how to move values from and to those areas
         
+        // There is a lowIndex and everything below that is sorted
+        // The lowIndex starts from array start
+        
+        // There is a lowerMidIndex and a higherMidIndex and everything in between them is sorted
+        // In the start these are in same index which is in the middle of the array
+        
+        // There is a highIndex and everything above that is sorted
+        // The highIndex starts from array end
+        
+        // The search areas are
+        // lowerSearchArea between lowIndex and lowerMidIndex
+        // higherSearchArea between higherMidIndex and highIndex
+        
+        // The sort areas are:
+        // lowSortArea that is in lowerIndex or below,
+        // lowMidSortArea that is the index below lowerMidIndex,
+        // midSortArea in between lowerMidIndex and higherMidIndex,
+        // highMidSortArea index above higherMidIndex,
+        // highSortArea in highIndex or above
+        
+        // The algorithm
+        // Take value from checkIndex (In the start: first index above lowIndex) and compare value of
+        // lowIndex => if smaller place to correct index in lowSortArea => lowIndex++
+        // lowerMidIndex => if lower than lowerMidIndex and highSearchArea => move to lowerMidIndex - 1 => lowerMidIndex--
+        // higherMidIndex => if higher than higherMidIndex and smaller than highIndex and lowerSearchArea => move to higherMidIndex + 1 => higherMidIndex++
+        // highIndex => if larger place to correct index in highSortArea => highIndex--
+        // After comparisons and possible movement take a new value for comparison
     }
 
     private void Print()
